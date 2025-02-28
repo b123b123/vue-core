be4b83d93c673806d6f3430bf8d0d3b164a45667
b123b123 <02684200@163.com>
2025-02-28 11:24:57 +0800

    feat: 注释

--- packages/shared/src/slotFlags.ts
+++ packages/shared/src/slotFlags.ts
@@ -1,28 +1,25 @@
+// 定义一个枚举类型 SlotFlags，用于表示不同类型的插槽标志
 export enum SlotFlags {
   /**
-   * Stable slots that only reference slot props or context state. The slot
-   * can fully capture its own dependencies so when passed down the parent won't
-   * need to force the child to update.
+   * 稳定的插槽，仅引用插槽属性或上下文状态。
+   * 插槽可以完全捕获其自身的依赖关系，因此当向下传递时，父组件不需要强制子组件更新。
    */
   STABLE = 1,
   /**
-   * Slots that reference scope variables (v-for or an outer slot prop), or
-   * has conditional structure (v-if, v-for). The parent will need to force
-   * the child to update because the slot does not fully capture its dependencies.
+   * 引用作用域变量（v-for 或外部插槽属性）或具有条件结构（v-if, v-for）的插槽。
+   * 父组件需要强制子组件更新，因为插槽不能完全捕获其依赖关系。
    */
   DYNAMIC = 2,
   /**
-   * `<slot/>` being forwarded into a child component. Whether the parent needs
-   * to update the child is dependent on what kind of slots the parent itself
-   * received. This has to be refined at runtime, when the child's vnode
-   * is being created (in `normalizeChildren`)
+   * 被转发到子组件中的 `<slot/>`。
+   * 父组件是否需要更新子组件取决于父组件本身接收到的插槽类型。
+   * 这需要在运行时进行细化，当子组件的 vnode 被创建时（在 `normalizeChildren` 中）。
    */
   FORWARDED = 3,
 }
 
-/**
- * Dev only
- */
+// 仅在开发环境中使用
+// 定义一个对象 slotFlagsText，将 SlotFlags 枚举值映射为对应的字符串表示
 export const slotFlagsText: Record<SlotFlags, string> = {
   [SlotFlags.STABLE]: 'STABLE',
   [SlotFlags.DYNAMIC]: 'DYNAMIC',
